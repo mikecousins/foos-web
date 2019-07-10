5bf8b6991abbbce52fbb30096ff5ae80c1cd5516
Mike Cousins <mike@mikecousins.com>
2019-07-10 16:47:46 -0600

    Styling bars better

--- src/components/DriveBar.tsx
+++ src/components/DriveBar.tsx
@@ -8,18 +8,31 @@ const DriveContainer = styled.div`
   width: 100%;
   background-color: #F7FAFC;
   margin-bottom: 10px;
-  padding: 10px;
+  padding: 0;
   display: flex;
+  flex-direction: column;
 `;
 
-const DriveNumber = styled.div`
+const DriveBody = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: row;
+`;
+
+const DriveNumberContainer = styled.div`
   width: 50px;
+  text-align: center;
+  padding: 10px;
+`;
+
+const DriveNumber = styled.span`
   font-size: 36px;
   font-weight: bold;
 `;
 
 const DriveDetails = styled.div`
   flex: 1;
+  padding: 10px;
 `;
 
 type Props = {
@@ -28,15 +41,20 @@ type Props = {
 
 const DriveBar = ({ drive }: Props) => (
   <DriveContainer>
-    <DriveNumber>
-      {drive.number}
-    </DriveNumber>
-    <DriveDetails>
-      <DriveUsage free={drive.free} capacity={drive.capacity} maxCapacity={8000000000000} />
-      {drive.brand} - {drive.model}<br />
-      <Size value={drive.free} /> / <Size value={drive.capacity} /><br />
-      {drive.temperature}&deg;C
-    </DriveDetails>
+    <DriveUsage free={drive.free} capacity={drive.capacity} maxCapacity={8000000000000} />
+    <DriveBody>
+      <DriveNumberContainer>
+        <DriveNumber>
+          {drive.number}
+        </DriveNumber>
+      </DriveNumberContainer>
+      <DriveDetails>
+        {drive.brand} - {drive.model}<br />
+        <Size value={drive.free} /> / <Size value={drive.capacity} /><br />
+        {drive.temperature}&deg;C
+      </DriveDetails>
+    </DriveBody>
+
   </DriveContainer>
 );
 
