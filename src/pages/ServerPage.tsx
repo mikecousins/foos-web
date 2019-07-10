d1323f654a7a77009d8e56c1c5500345bb70f10b
Mike Cousins <mike@mikecousins.com>
2019-07-10 14:27:59 -0600

    Adding totals

--- src/pages/ServerPage.tsx
+++ src/pages/ServerPage.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import DriveBar from '../components/DriveBar';
 import { Server } from '../types/Server';
+import Size from '../components/Size';
 
 const ServerPage = () => {
   const [server, setServer] = useState<Server | null>(null);
@@ -16,9 +17,17 @@ const ServerPage = () => {
     return <div>Loading...</div>;
     // return <FontAwesomeIcon icon={faSpinner} spin />;
   }
+
+  let totalCapacity = 0;
+  let totalFree = 0;
+  server.drives.forEach(drive => {
+    totalCapacity += drive.capacity;
+    totalFree += drive.free;
+  })
   const drives = server.drives.map((drive: any, index: number) => <DriveBar drive={drive} key={index} />);
   return (
     <React.Fragment>
+      <h1><Size value={totalFree} /> / <Size value={totalCapacity} /></h1>
       {drives}
     </React.Fragment>
   );
